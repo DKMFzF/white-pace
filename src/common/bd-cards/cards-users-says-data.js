72fad4b774ff85cdf926f2955221cc372d006314
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-10-31 15:29:12 +0700

    feat(btn users0say)

--- src/common/bd-cards/cards-users-says-data.js
+++ src/common/bd-cards/cards-users-says-data.js
@@ -1,43 +1,43 @@
 const dataCardUserSays = [
   {
-    headerVideo: {
-        avif: '',
-        webm: '',
-        mp4: ''
+    name: 'avatar-1',
+    imgVideo: {
+      src: './images/png/card-user-avatar-1.png',
+      alt: 'avatar user'
     },
     content:
-      '“If you haven’t tried whitepace yet, you need to give it a shot for your next event. It’s so easy and intuitive to get a new event setup and if you need any help their customer service is seriously amazing.”',
+      '“If you havent tried whitepace yet, I strongly recommend that you pay attention to it for organizing your next event. The platform is so simple and intuitive that you can easily and quickly set up a new event. And if you have any questions or need help, their customer support team is amazing - they are always ready to help and do everything possible to make your event go smoothly and successfully.”',
     footer: {
       name: 'Jessie Owner',
-      company: 'Founder, XYZ Company',
+      company: 'XYZ',
       estimation: '5',
     },
   },
   {
-    headerVideo: {
-        avif: '',
-        webm: '',
-        mp4: ''
+    name: 'avatar-2',
+    imgVideo: {
+      src: './images/png/card-user-avatar-2.png',
+      alt: 'avatar user'
     },
     content:
-      '“If you haven’t tried whitepace yet, you need to give it a shot for your next event. It’s so easy and intuitive to get a new event setup and if you need any help their customer service is seriously amazing.”',
-    footer: {
+      '“I used whitepace to organize my event, and it was a great choice. The platform is intuitive, setting up the event took only a few minutes. It was very easy to manage all aspects, from the registration of participants to the schedule. The support service also proved to be on top — they quickly answered questions and helped solve problems. I recommend it to anyone who is looking for a convenient solution for organizing events!”',
+     footer: {
       name: 'Mike Adinson',
       company: 'Google',
       estimation: '4',
     },
   },
   {
-    headerVideo: {
-        avif: '',
-        webm: '',
-        mp4: ''
+    name: 'avatar-3',
+    imgVideo: {
+      src: './images/png/card-user-avatar-3.png',
+      alt: 'avatar user'
     },
-    content:
-      '“If you haven’t tried whitepace yet, you need to give it a shot for your next event. It’s so easy and intuitive to get a new event setup and if you need any help their customer service is seriously amazing.”',
+    content: 
+      '“Whitepace is a godsend for event organizers! I used it for my last event and it was just great. The interface is very friendly, and I easily set up the event. I especially want to mention the work of the support service: they were in touch and helped me with several questions, which made the process even more comfortable. If you want to save time and effort on organization, be sure to try whitepace!”',
     footer: {
       name: 'Igor Petrovchenko',
-      company: 'Amazon',
+      company: 'Yandex',
       estimation: '5',
     },
   },
--- src/common/card-user-says/card-user-says.js
+++ src/common/card-user-says/card-user-says.js
@@ -2,21 +2,70 @@ function createCardUserSays(card) {
     const cardTemplate = document.querySelector('#card-user-says-template').content;
     const cardItem = cardTemplate.querySelector('.section-content__list-item-card-user-says').cloneNode(true);
 
-    const videoAvif = cardItem.querySelector('#CardUserSaysVideoAvif');
-    const videoWebm = cardItem.querySelector('#CardUserSaysVideoWebm');
-    const videoMp4 = cardItem.querySelector('#CardUserSaysVideoMp4');
-    videoAvif.src = card.headerVideo.avif;
-    videoWebm.src = card.headerVideo.webm;
-    videoMp4.src = card.headerVideo.mp4;
+    const imgAvatar = cardItem.querySelector('#cardUserSaysAvatar');
+    imgAvatar.src = card.imgVideo.src;
+    imgAvatar.alt = card.imgVideo.alt;
 
     const cardContent = cardItem.querySelector('.card-user-says__comment');
     cardContent.textContent = card.content;
 
     const cardNameUser = cardItem.querySelector('.card-user-says__title-user-name');
-    const cardUserPlaceWork = cardItem.querySelector('.card-user-says__user-place-work');
-    // const cardEstimationUser = cardItem.querySelector('.card-user-says__user-estimation');
     cardNameUser.textContent = card.footer.name;
-    cardUserPlaceWork.textContent = card.footer.company;
+
+    addClassInCardUserSays(card, cardItem);
     
     return cardItem;
+}
+
+function addClassInCardUserSays(card, cardItem) {
+    const cardUserPlaceWork = cardItem.querySelector('.card-user-says__container-user-place-work');
+    switch (card.name) {
+        case 'avatar-1':
+            cardItem.querySelector('.card-user-says__img').classList.add('card-user-says__img_avatar-inset-first');
+            addClassCompanyInCardUsers(
+                card, 
+                cardUserPlaceWork, 
+                'card-user-says__user-place-work_x-letter-first-card', 
+                'card-user-says__user-place-work_y-letter-first-card', 
+                'card-user-says__user-place-work_z-letter-first-card'
+            );
+            break;
+        case 'avatar-2':
+            cardItem.querySelector('.card-user-says__img').classList.add('card-user-says__img_avatar-inset-second');
+            addClassCompanyInCardUsers(
+                card, 
+                cardUserPlaceWork, 
+                'card-user-says__user-place-work_g-letter-second-card',
+                'card-user-says__user-place-work_o-letter-second-card', 
+                'card-user-says__user-place-work_o-double-letter-second-card',
+                'card-user-says__user-place-work_g-double-letter-second-card',
+                'card-user-says__user-place-work_l-letter-second-card',
+                'card-user-says__user-place-work_e-letter-second-card',
+            );
+            break;
+        case 'avatar-3':
+            cardItem.querySelector('.card-user-says__img').classList.add('card-user-says__img_avatar-inset-third');
+            addClassCompanyInCardUsers(
+                card, 
+                cardUserPlaceWork, 
+                'card-user-says__user-place-work_y-letter-third-card', 
+                'card-user-says__user-place-work_a-letter-third-card', 
+                'card-user-says__user-place-work_n-letter-third-card',
+                'card-user-says__user-place-work_d-letter-third-card',
+                'card-user-says__user-place-work_e-letter-third-card',
+                'card-user-says__user-place-work_x-letter-third-card'
+            );
+            break;
+    }
+}
+
+function addClassCompanyInCardUsers(card, cardUserPlaceWork, ...className) {
+    const textLine = card.footer.company.split("");
+    if (textLine.length !== className.length) return console.log('ERROR: NONE CARD');
+    textLine.forEach((element, index) => {
+        const newElement = document.createElement('span');
+        newElement.classList.add(className[index]);
+        newElement.textContent = element;
+        cardUserPlaceWork.append(newElement);
+    });
 }
