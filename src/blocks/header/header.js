43da6ef77ec589fa85b535f8da19822929a62d2e
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-11-01 14:48:38 +0700

    feat(header)

---
+++ src/blocks/header/header.js
@@ -0,0 +1,12 @@
+let lastScrollTop = 0;
+const header = document.querySelector('#header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        header.style.top = "-97px";
+    } else {
+        header.style.top = "0";
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
